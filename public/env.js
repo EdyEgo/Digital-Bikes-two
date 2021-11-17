86296b984d3c8141a343e758e0257ac10e53550e
EdyEgo <prodan.septimiu@gmail.com>
2021-11-17 17:55:05 +0200

    trying redirects

--- public/env.js
+++ public/env.js
@@ -183,6 +183,7 @@ window.env = {
   "npm_package_description": "This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).",
   "npm_package_eslintConfig_extends_0": "react-app",
   "npm_package_eslintConfig_extends_1": "react-app/jest",
+  "npm_package_gitHead": "40bb860f1a0513a953b66c7b2447a18a6e830b18",
   "npm_package_name": "digital-bikes",
   "npm_package_private": "true",
   "npm_package_readmeFilename": "README.md",
